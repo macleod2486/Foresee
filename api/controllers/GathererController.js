eb5af179c92f84648eccd6460400a478c35edbf4
macleod2486 <macleod2486@gmail.com>
2015-04-01 02:01:42 -0500

    Created basic api

---
+++ api/controllers/GathererController.js
@@ -0,0 +1,11 @@
+/**
+ * GathererController
+ *
+ * @description :: Server-side logic for managing gatherers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
