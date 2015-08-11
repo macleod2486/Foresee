8b5600ccdea8c597847f4bb1b083eefb46d2b69d
macleod2486 <macleod2486@gmail.com>
2015-08-11 01:31:38 -0500

    Routed home page

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,17 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports =
+{
+
+	list: function(req,res)
+	{
+		res.view();
+	}
+
+};
+
