ebc50c54117f317f94faf5e8e42417bc313c7c11
macleod2486 <macleod2486@gmail.com>
2015-09-06 01:36:30 -0500

    Set search to look for unique cards

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -11,13 +11,38 @@ module.exports =
 	{
 		if(req.method == 'POST')
 		{
-			sails.models.gatherer.find({nameOfCard:{'like' : '%'+req.param('search')+'%'}, sort: 'nameOfCard asc'}).exec(
+			sails.models.gatherer.find({nameOfCard:{'like' : '%'+req.param('search')+'%'}, sort: 'nameOfCard asc'},' distinct').exec(
 					
-						function(error, cards)
+					function(error, cards)
+					{
+						var uniqueList = [];
+						var temp;
+						var isInList = false;
+
+						for(var index = 0; index < cards.length; index++)
 						{
-							sails.log("Number found "+cards.length);
-							res.view({cards: cards});
+							temp = cards[index];
+
+							for(var index2 = 0; index2 < uniqueList.length; index2++)
+							{
+								if(uniqueList[index2]['nameOfCard'] == temp['nameOfCard'] && uniqueList[index2]['set'] == temp['set'])
+								{
+									isInList = true;
+								}
+							}
+
+							if(!isInList)
+							{
+								uniqueList.push(temp);
+							}
+
+							isInList = false;
+
 						}
+
+						sails.log("Number found "+uniqueList.length);
+						res.view({cards: uniqueList});
+					}
 
 					);
 		}
