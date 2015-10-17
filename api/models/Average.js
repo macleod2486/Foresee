cc9e7b3df17238f65d00a9b34aa07261f408f681
macleod2486 <macleod2486@gmail.com>
2015-10-16 22:57:06 -0500

    Created average model

---
+++ api/models/Average.js
@@ -0,0 +1,33 @@
+/**
+* Average.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes:
+  {
+	  nameOfCard:
+	  {
+		  type: 'string'
+	  },
+
+	  lowPrice:
+	  {
+		  type: 'string'
+	  },
+
+	  mediumPrice:
+	  {
+		  type: 'string'
+	  },
+
+	  highPrice:
+	  {
+		  type: 'string'
+	  }
+  }
+};
+
