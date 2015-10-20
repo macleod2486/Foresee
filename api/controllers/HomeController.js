3de89464cfd4ebb0e09816f638cc3f098d9f0746
macleod2486 <macleod2486@gmail.com>
2015-10-19 22:59:01 -0500

    Redid search to look up out of average model

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -11,37 +11,12 @@ module.exports =
 	{
 		if(req.method == 'POST')
 		{
-			sails.models.gatherer.find({nameOfCard:{'like' : '%'+req.param('search')+'%'}, sort: 'nameOfCard asc'},' distinct').exec(
+			sails.models.average.find({nameOfCard:{'like' : '%'+req.param('search')+'%'}, sort: 'nameOfCard asc'},' distinct').exec(
 					
 					function(error, cards)
 					{
-						var uniqueList = [];
-						var temp;
-						var isInList = false;
-
-						for(var index = 0; index < cards.length; index++)
-						{
-							temp = cards[index];
-
-							for(var index2 = 0; index2 < uniqueList.length; index2++)
-							{
-								if(uniqueList[index2]['nameOfCard'] == temp['nameOfCard'] && uniqueList[index2]['set'] == temp['set'])
-								{
-									isInList = true;
-								}
-							}
-
-							if(!isInList)
-							{
-								uniqueList.push(temp);
-							}
-
-							isInList = false;
-
-						}
-
-						sails.log("Number found "+uniqueList.length);
-						res.view({cards: uniqueList});
+						sails.log("Number found "+cards.length);
+						res.view({cards: cards});
 					}
 
 					);
